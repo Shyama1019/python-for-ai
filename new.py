fbbcaae3c4446c7c3490ea72dc71c8e50a946bef
Shyama1019 <shyama1019@gmail.com>
2026-07-01 15:23:19 +0530

    my learning

---
+++ new.py
@@ -0,0 +1,68 @@
+person = {
+    "name" : "Sivagami",
+    "age" : 42,
+    "city" : "Coimbatore"
+}
+
+print(person["name"])
+print(person["age"])
+print(person["city"])
+
+print(person["name"], "is", person["age"], "years and lives in", person["city"])
+
+
+students = [
+    {"Name": "Sivagami", "age":42, "mark": 65},
+    {"Name": "Mohan", "age":43, "mark": 72},
+    {"Name": "Akshanth", "age":15, "mark": 91},
+    {"Name": "Dhanvanth", "age": 10, "mark": 87}
+]
+def get_grade(mark):
+    if mark > 90:
+        return("Grade A")
+    elif mark>80:
+        return("Grade B")
+    elif mark > 70:
+        return("Grade C")
+    else:
+        return("Grade D")
+
+for student in students:
+    grade = get_grade(student["mark"])
+
+    print(student["Name"], "scored", student["mark"], "and got", grade)
+
+
+expenses = [
+    {"item": "Rice", "price": 65, "quantity": 5},
+    {"item": "sugar", "price":43, "quantity": 3},
+    {"item": "book", "price": 125, "quantity": 7},
+    {"item": "oil", "price": 90, "quantity": 5}
+]
+highest_cost = 0
+expensive_item = ""
+grand_total = 0
+chepest_cost = 3000
+chepest_item = ""
+
+def get_total(price,quantity):
+    return price*quantity
+
+for expense in expenses:
+    total_exp = get_total(expense["price"], expense["quantity"])
+    print(expense["item"], expense ["price"], "*" ,expense ["quantity"], "=", total_exp)
+    grand_total+= total_exp
+    if total_exp > highest_cost:
+        highest_cost = total_exp
+        expensive_item = expense["item"]
+    
+    if total_exp < chepest_cost:
+        chepest_cost = total_exp
+        chepest_item = expense["item"]
+
+
+print("Grand_Total:", grand_total)   
+print("Highest_Cost:", highest_cost)
+print("Expensive_Item:",expensive_item )
+print("Chepest_Item:", chepest_item)
+print("Chepest_cost:", chepest_cost)
