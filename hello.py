e91e19fa4b0584b04c832154a49f9a4cacdddd94
Shyama1019 <shyama1019@gmail.com>
2026-07-01 11:30:17 +0530

    new

---
+++ hello.py
@@ -0,0 +1,53 @@
+
+
+number = int(input("Type a number:"))23
+
+
+if number% 2 == 0:
+    print("Even")
+else:
+    print("odd")
+
+
+number = int(input("Type a number:"))
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+
+
+range (1,6)
+print (range)
+
+
+for i in range (1,6):
+    print(i)
+
+
+for i in range (1,11):
+    if i % 2 == 0:
+        print(i)
+
+
+for i in range (1,21):
+    if i % 3 == 0 and i% 5 == 0:
+        print("Fizzbuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i% 5 == 0:
+        print ("Buzz")
+
+    else:
+        print (i)
+
+
+
+
+
+
+    
+
+    
